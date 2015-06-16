0423cd20826fec712c045ceeb81a0b84303b85c2
ankush-garg <agarg.punjabi@gmail.com>
2015-06-16 10:36:55 -0700

    Adding commented code

--- server.js
+++ server.js
@@ -26,10 +26,10 @@ var modules = {
   'dummy-module': require('./dummy-module')
 }
 
-// if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
   modules['netsuite-zendesk-connector'] = require('netsuite-zendesk-connector');
   modules['shopify-netsuite-connector'] = require('shopify-netsuite-connector');
-// }
+}
 
 var port = nconf.get('TEST_INTEGRATOR_EXTENSION_PORT') || 80;
 

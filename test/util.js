597f1ab2e662da2b94ce053d16e647ffec0c33bf
Vinay Kumar <vinaykhebbar@gmail.com>
2017-01-09 11:17:53 +0530

    adding test case for support for instances (#76)
    * added comment
    
    * adding test case for support for instances

--- test/util.js
+++ test/util.js
@@ -1,6 +1,6 @@
 var Extension = require('../lib/extension')
 var testExtension = new Extension()
-var testModule = require('./testModule')
+var testModules = require('./testModules')
 var should = require('should')
 
 exports.callFunction = function (options, extensionProperties, callback) {
@@ -9,10 +9,10 @@ exports.callFunction = function (options, extensionProperties, callback) {
 
 exports.createMockExtension = function (diy, connector, callback) {
   var config = {}
-  config.diy = diy ? testModule : undefined
+  config.diy = diy ? testModules.testModule : undefined
   var connectors = {
-    '9ce44f88a25272b6d9cbb430ebbcfcf1': testModule,
-    '6a4b9e817fb9f522dbd012f642855a03': testModule
+    '9ce44f88a25272b6d9cbb430ebbcfcf1': testModules.testModule,
+    '6a4b9e817fb9f522dbd012f642855a03': testModules.testInstance
   }
   config.connectors = connector ? connectors : undefined
   testExtension.loadConfiguration(config, function (e) {
